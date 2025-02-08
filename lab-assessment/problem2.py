9ee01166cb42eff9336c288fddee8550e2975376
ahmad-cod <ahmadadewale800@gmail.com>
2025-02-08 21:34:48 +0100

    Refactor variable name in even numbers list comprehension for clarity

--- lab-assessment/problem2.py
+++ lab-assessment/problem2.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-even_numbers = [n for n in numbers if n % 2 == 0]
+even_numbers = [num for num in numbers if num % 2 == 0]
 
 print(even_numbers)
